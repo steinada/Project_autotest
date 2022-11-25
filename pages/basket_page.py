97e9f0266ac0254ac227be95c7318adf13d5ae58
steinada <110931286+steinada@users.noreply.github.com>
2022-11-25 23:19:14 +0600

    add new tests

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,7 +4,7 @@ from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
     def should_be_message_empty_basket(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "Basket is not empty"
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "Basket isn't empty"
 
     def should_not_be_order_button(self):
-        assert self.is_disappeared(*BasketPageLocators.CHECKOUT_BUTTON), "Basket is not empty"
+        assert self.is_disappeared(*BasketPageLocators.CHECKOUT_BUTTON), "Basket isn't empty"
